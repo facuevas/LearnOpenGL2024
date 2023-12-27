c0fb32db6cd7bb118dd15487480957e00585b771
Francis Angelo Cuevas <franciscuevas4@gmail.com>
2023-12-27 13:40:52 -0800

    Hello Window. Also downgrade to OpenGL 3.3

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -1,9 +1,64 @@
-#include <iostream>
+// clang-format off
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+// clang-format on
+
+// Callbacks
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height);
+void ProcessInput(GLFWwindow* window);
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	// GLFW Window Hints
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// MacOS-specific Window Hint
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE, GL_TRUE);
+#endif
+
+	// Instantiate the object window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to instantiate the GLFW window." << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// Init GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return 1;
+	}
+
+	// Viewport
+	glViewport(0, 0, 800, 600);
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	// Cleanup
+	glfwTerminate();
 	return 0;
+}
+
+void FramebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void ProcessInput(GLFWwindow* window)
+{
 }
