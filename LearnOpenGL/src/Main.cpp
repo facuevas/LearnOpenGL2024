79cb675d4a606838b5ca081d0f16b7068082ddfb
Francis Angelo Cuevas <franciscuevas4@gmail.com>
2023-12-27 14:30:53 -0800

    Hello Rectangle

--- LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/src/Main.cpp
@@ -14,10 +14,17 @@ const unsigned int SCREEN_HEIGHT = 600;
 
 // Triangle vertices
 
-const float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f};
+float vertices[] = {
+		0.5f, 0.5f, 0.0f,  // top right
+		0.5f, -0.5f, 0.0f, // bottom right
+		-0.5f, -0.5f, 0.0f,// bottom left
+		-0.5f, 0.5f, 0.0f  // top left
+};
+unsigned int indices[] = {
+		// note that we start from 0!
+		0, 1, 3,// first triangle
+		1, 2, 3 // second triangle
+};
 
 // Hacking in the shaders for now
 const char* vertexShaderSource = "#version 330 core\n"
@@ -114,14 +121,25 @@ int main()
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	unsigned int VBO, VAO;
+	// Vertex Buffer Objects
+	// Vertex Array Objects
+	// Element Buffer Objects
+	unsigned int VBO, VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
+
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
 	glEnableVertexAttribArray(0);
+
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
 
@@ -138,7 +156,7 @@ int main()
 		// Triangle
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		// Swap The Buffers
 		glfwSwapBuffers(window);
@@ -146,6 +164,12 @@ int main()
 	}
 
 	// Cleanup
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+	glDeleteProgram(shaderProgram);
+
+	// Terminate
 	glfwTerminate();
 	return 0;
 }
